a2532162f7b57cabe66e2b9d9d3508aec6c53909
Adrián Valera Román <adrianvalrom@gmail.com>
2022-11-14 18:49:40 +0100

    Initial commit

---
+++ src/test/java/example/ExampleShould.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExampleShould {
+    @Test
+    public void
+    be_true() {
+        assertTrue(true);
+    }
+
+}
